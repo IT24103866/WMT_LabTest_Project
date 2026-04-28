65cdd786451f1d4175775dd3f0ac87bc62480031
IT24103866 <IT24103866@my.sliit.lk>
2026-04-28 10:31:26 +0530

    ADD THE url

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://itemmanagement-a6bvavf0degkgcem.southeastasia-01.azurewebsites.net/api",
 });
 
 export const getItems = () => API.get("/items");
